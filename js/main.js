184b1858e6ab4b7b9de436989d62e7e88dad5e5f
MariusTBjercke <marius@bjerckemedia.no>
2021-08-24 00:48:28 +0200

    Combined all team members code and added "game" functionalities

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@ var geirImg = "img/geir.png";
 var activePlayer = "geir";
 var roundCheck = true;
 var counter = 0;
+var roundCounter = 1;
 var winningText = document.getElementById("winning-text");
+var finalWinningText = document.getElementById("final-winning-text");
+var nextButton = document.getElementById("next-button");
+var restartButton = document.getElementById("restart-button");
 
-// Utfører spillrunde. Sjekker spiller etter tur (altså Geir eller Eskil) ved å bruke boolean. 
-// Sjekker om aktiv spiller er lik Geir, om Geir da er den aktive spilleren så endres aktiv spiller til Eskil. 
-// Vi bruker en variabel for å sjekke om en runde er utført ved å gi den true/false og sjekker runde etter dette. Setter omvendt verdi etter fullført runde.
+// Utfører spillrunde. Sjekker spiller etter tur (altså Geir og Eskil) ved å bruke boolean (true eller false)
+// Sjekker om aktiv spiller er Geir, om Geir da er den aktive spilleren så endres aktiv spiller til Eskil etter klikk
+// Vi bruker en variabel for å sjekke om en runde er utført ved å gi den true/false og sjekker runde etter dette. Setter omvendt verdi etter fullført runde
 function playerTurn() {
     if (activePlayer === "geir" && roundCheck === true) {
         activePlayer = "eskil";
@@ -30,10 +34,33 @@ function changeOnClick(element) {
         playerTurn();
     }
 
-    if (counter === 8) {
+    if (counter === 8 && roundCounter < 3) {
         winningText.style.display = "inherit";
+        roundCounter++;
+    } else if (counter === 8 && roundCounter === 3) {
+        finalWinningText.style.display = "inherit";
     } else {
         counter++;
     }
 
+}
+
+// Gå til neste runde ved å trykke på knapp
+nextButton.onclick = function() {
+    if (roundCounter === 2) {
+        document.getElementById("round-1").style.display = "none";
+        document.getElementById("round-2").style.display = "inherit";
+        winningText.style.display = "none";
+        counter = 0;
+    } else if (roundCounter === 3) {
+        document.getElementById("round-2").style.display = "none";
+        document.getElementById("round-3").style.display = "inherit";
+        winningText.style.display = "none";
+        counter = 0;
+    }
+}
+
+// Restart spill, omdiriger nettleser til index.html (tilbake til forsiden)
+restartButton.onclick = function() {
+    document.location.href = "index.html";
 }
