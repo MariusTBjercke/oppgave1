04e0b1c29922801afec03f4243de5d8553cd96b2
MariusTBjercke <marius@bjerckemedia.no>
2021-08-24 13:19:37 +0200

    Removed unnecessary code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var terjeImg = "img/terje.png";
 var eskilImg = "img/eskil.png";
 var geirImg = "img/geir.png";
 var activePlayer = "geir";
-var roundCheck = true;
 var counter = 0;
 var roundCounter = 1;
 var winningText = document.getElementById("winning-text");
@@ -11,27 +10,14 @@ var finalWinningText = document.getElementById("final-winning-text");
 var nextButton = document.getElementById("next-button");
 var restartButton = document.getElementById("restart-button");
 
-// Utfører spillrunde. Sjekker spiller etter tur (altså Geir og Eskil) ved å bruke boolean (true eller false)
-// Sjekker om aktiv spiller er Geir, om Geir da er den aktive spilleren så endres aktiv spiller til Eskil etter klikk
-// Vi bruker en variabel for å sjekke om en runde er utført ved å gi den true/false og sjekker runde etter dette. Setter omvendt verdi etter fullført runde
-function playerTurn() {
-    if (activePlayer === "geir" && roundCheck === true) {
-        activePlayer = "eskil";
-        roundCheck = false;
-    } else {
-        activePlayer = "geir";
-        roundCheck = true;
-    }
-}
-
 // Funksjon som endrer til bilde av Geir og Eskil annenhver gang
 function changeOnClick(element) {
     if (activePlayer === "geir") {
         element.style.backgroundImage = 'url("' + geirImg + '")';
-        playerTurn();
+        activePlayer = "eskil";
     } else {
         element.style.backgroundImage = 'url("' + eskilImg + '")';
-        playerTurn();
+        activePlayer = "geir";
     }
 
     if (counter === 8 && roundCounter < 3) {
@@ -40,6 +26,7 @@ function changeOnClick(element) {
     } else if (counter === 8 && roundCounter === 3) {
         finalWinningText.style.display = "inherit";
     } else {
+        // Teller oppover for hvert trykk (+1)
         counter++;
     }
 
@@ -47,10 +34,13 @@ function changeOnClick(element) {
 
 // Gå til neste runde ved å trykke på knapp
 nextButton.onclick = function() {
+    // Hvis roundCounter er 2 så setter vi style "display" til "none" på runden som var og setter "display" til "inherit" på neste runde for at den skal vises.
     if (roundCounter === 2) {
         document.getElementById("round-1").style.display = "none";
         document.getElementById("round-2").style.display = "inherit";
+        // Skjuler "Runden ble fullført!" teksten
         winningText.style.display = "none";
+        // Nullstiller counter slik at man kan starte spillet på nytt
         counter = 0;
     } else if (roundCounter === 3) {
         document.getElementById("round-2").style.display = "none";
